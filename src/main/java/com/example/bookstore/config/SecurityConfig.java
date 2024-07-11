352f616a656440066d615ae72237b188c3f790cc
nastasiiaKulinich <kussik2004@gmail.com>
2024-07-11 16:00:56 +0300

    added constant BEARER_PREFIX and used constant HttpHeaders.AUTHORIZATION in method getToken()

--- src/main/java/com/example/bookstore/config/SecurityConfig.java
+++ src/main/java/com/example/bookstore/config/SecurityConfig.java
@@ -9,7 +9,6 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -21,7 +20,6 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 @EnableMethodSecurity
 @RequiredArgsConstructor
 @Configuration
-@EnableWebSecurity
 public class SecurityConfig {
     private final UserDetailsService userDetailsService;
     private final JwtAuthenticationFilter jwtAuthenticationFilter;
