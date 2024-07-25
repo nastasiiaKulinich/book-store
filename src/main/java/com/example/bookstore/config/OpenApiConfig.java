13aa76bcc0c72cd511221bdf7f5e0c0067d7bf1c
nastasiiaKulinich <kussik2004@gmail.com>
2024-07-25 14:20:18 +0300

    added String constants in OpenApiConfig class

--- src/main/java/com/example/bookstore/config/OpenApiConfig.java
+++ src/main/java/com/example/bookstore/config/OpenApiConfig.java
@@ -9,14 +9,18 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class OpenApiConfig {
+    private static final String BEARER_AUTH = "BearerAuth";
+    private static final String BEARER = "bearer";
+    private static final String JWT = "JWT";
+
     @Bean
     public OpenAPI customOpenApi() {
         return new OpenAPI()
-                .components(new Components().addSecuritySchemes("BearerAuth",
+                .components(new Components().addSecuritySchemes(BEARER_AUTH,
                         new SecurityScheme()
                                 .type(SecurityScheme.Type.HTTP)
-                                .scheme("bearer")
-                                .bearerFormat("JWT")))
-                .addSecurityItem(new SecurityRequirement().addList("BearerAuth"));
+                                .scheme(BEARER)
+                                .bearerFormat(JWT)))
+                .addSecurityItem(new SecurityRequirement().addList(BEARER_AUTH));
     }
 }
